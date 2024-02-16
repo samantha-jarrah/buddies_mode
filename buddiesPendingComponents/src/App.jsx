a2fb9cd099d2f80dbabc0077d27f3443adb8b64a
mendalej <107884708+mendalej@users.noreply.github.com>
2024-02-16 10:03:03 -0600

    alejandra's updates

--- buddiesPendingComponents/src/App.jsx
+++ buddiesPendingComponents/src/App.jsx
@@ -3,15 +3,25 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { SearchBar } from './components/buddiesModeSearch';
+import { SearchResultsList } from './components/SearchResultsList';
+
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [results, setResults] = useState([]);
 
   return (
-    <>
-    </>
+    <div className='App'>
+      <div className="search-bar">
+        <SearchBar setResults ={setResults} />
+        <SearchResultsList results={results}/>
+      </div>
+    </div>
   )
 }
 
-export default App
+export default App;
+
+
---
+++ buddiesPendingComponents/src/components/SearchBar/SearchResultsList.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./SearchResultsList.css";
+
+export const SearchResultsList = ({results}) => {
+    return (
+        <div className="results-list">
+        {
+            results.map((result, id) => {
+                return <div key={id}>
+                    return <SearchResult  result={result} key={id} />;
+                </div>;
+            })
+        }
+        </div>
+    );
+};
+
+export default SearchResultsList;
